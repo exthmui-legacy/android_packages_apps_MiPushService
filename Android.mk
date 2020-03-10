92c3d49202bd6ada2cde047a4cba20599209ea0d
cjybyjk <cjybyjk@gmail.com>
2020-03-10 16:42:01 +0800

    MiPush doesn't require privapp-permissions [1/2]

--- Android.mk
+++ Android.mk
@@ -8,12 +8,4 @@ LOCAL_CERTIFICATE := platform
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
 LOCAL_DEX_PREOPT := false
-LOCAL_REQUIRED_MODULES := mipushservice.permissions
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE := mipushservice.permission
-LOCAL_MODULE_CLASS := ETC
-LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)/permissions
-LOCAL_SRC_FILES := privapp-permissions-mipushservice.xml
 include $(BUILD_PREBUILT)
